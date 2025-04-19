fd86995c0827c99f12c0544b124ccf22f0db2afe
DodgeTheDarins <josh.online.when@gmail.com>
2025-04-18 20:23:41 -0600

    STUFF

--- src/notifier.py
+++ src/notifier.py
@@ -1,3 +1,19 @@
+# This file is part of Minecraft Server Checker.
+#
+# Minecraft Server Checker is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Minecraft Server Checker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Minecraft Server Checker. If not, see <https://www.gnu.org/licenses/>.
+
+
 from plyer import notification
 
 class Notifier:
@@ -11,4 +27,4 @@ class Notifier:
             message=message,
             app_name="Minecraft Server Checker",
             timeout=10  # Notification will disappear after 10 seconds
-        )
+        ) # type: ignore
